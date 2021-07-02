cd5c219d9678e9eff01db83b132937e79ad02bea
RamaPratt <ramapratt@gmail.com>
2021-07-01 19:19:37 -0700

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(password: params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back, #{user.name}!"
     else
-      flash.now[:alert] = "Invalid Email/Password combination."
+      flash.now[:alert] = "Invalid email/password combination!"
       render :new
     end
   end
